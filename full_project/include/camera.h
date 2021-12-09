a8a60df5892e0a50018225ca73f6c6c377d2d571
goldh0rse <klas.g.holmberg@gmail.com>
2021-12-09 19:12:13 +0100

    Implemented camera class

--- full_project/include/camera.h
+++ full_project/include/camera.h
@@ -8,28 +8,46 @@
 
 using namespace std;
 
-enum direction {FORWARD = 0, BACKWARD, LEFT, RIGHT, UP, DOWN};
+enum direction { FORWARD = 0, BACKWARD, LEFT, RIGHT, UP, DOWN };
+const float pi_f = 3.1415926f;
 
 class Camera {
 public:
   // Constructors & Destructor
-  Camera(void);
-  ~Camera(void);
+  Camera(glm::vec3 startPos, glm::vec3 worldUp, glm::vec3 startFace);
+  ~Camera(void){};
 
-  void updateCamera();
-  void moveCamera();
+  // Functions
+  void updateCameraFacing(GLfloat offsetX, GLfloat offsetY);
+  void moveCamera(const int direction);
+  inline glm::mat4 getViewMatrix(void) { return this->ViewMatrix; }
+  inline glm::vec3 getCamPosition(void) { return this->camPosition; }
+  void updateViewMatrix(void);
 
 private:
+  // Functions
+
   // Attributes
   glm::mat4 ViewMatrix;
+  const glm::vec3 startPos;
+  const glm::vec3 worldUp;
+  const glm::vec3 startFace;
   glm::vec3 camPosition;
   glm::vec3 camFace;
   glm::vec3 camUp;
   glm::vec3 camRight;
 
-  glm::vec3 worldUp;
+  GLfloat pitch;
+  GLfloat yaw;
+  GLfloat camSensitivity;
+  GLfloat camSpeed;
 
-
+  float fov;
+  float nearPlane; // Due to where we want clipping to be
+  float farPlane; // Test on itchy, if slow dump to 100.f
+  float top;
+  float obliqueScale;
+  float obliqueAngleRad;
 };
 
 #endif
--- full_project/src/camera.cpp
+++ full_project/src/camera.cpp
@@ -1,1 +1,82 @@
 #include "camera.h"
+
+// Constructors & Destructors
+Camera::Camera(glm::vec3 startPos, glm::vec3 worldUp, glm::vec3 startFace)
+  :
+  startPos(startPos),
+  worldUp(worldUp),
+  startFace(startFace){
+    this->camPosition = this->startPos;
+    this->camFace = this->startFace;
+    this->camRight = glm::vec3(0.f);
+    this->camRight = glm::normalize(glm::cross(this->camFace, this->worldUp));
+    this->camUp = glm::normalize(glm::cross(this->camRight, this->camFace));
+
+    this->pitch = 0.f;
+    this->yaw = -90.f;
+    this->camSensitivity = 0.1f;
+    this->camSpeed = 0.05f;
+}
+
+// Public
+void Camera::moveCamera(const int direction){
+  //Update position vector
+	switch (direction){
+	case FORWARD:
+		this->camPosition += this->camFace * this->camSpeed;
+		break;
+	case BACKWARD:
+		this->camPosition -= this->camFace * this->camSpeed;
+		break;
+	case LEFT:
+		this->camPosition -= this->camRight * this->camSpeed;
+		break;
+	case RIGHT:
+		this->camPosition += this->camRight * this->camSpeed;
+		break;
+  case UP:
+    this->camPosition += this->camUp * this->camSpeed;
+    break;
+  case DOWN:
+    this->camPosition -= this->camUp * this->camSpeed;
+    break;
+	default:
+    // Well this is dumb, throw error maybe?
+		break;
+	}
+}
+
+void Camera::updateCameraFacing(GLfloat offsetX, GLfloat offsetY){
+  this->pitch += static_cast<GLfloat>(offsetY) * this->camSensitivity;
+  this->yaw += static_cast<GLfloat>(offsetX) * this->camSensitivity;
+
+  /* Prevent Gimbal lock */
+  if (this->pitch > 80.f){
+    this->pitch = 80.f;
+  }	else if (this->pitch < -80.f){
+    this->pitch = -80.f;
+  }
+
+  if (this->yaw > 360.f || this->yaw < -360.f){
+      this->yaw = 0.f;
+  }
+
+  this->camFace.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+  this->camFace.y = sin(glm::radians(this->pitch));
+  this->camFace.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+
+  this->camFace = glm::normalize(this->camFace);
+  this->camRight = glm::normalize(glm::cross(this->camFace, this->worldUp));
+  this->camUp = glm::normalize(glm::cross(this->camRight, this->camFace));
+
+  this->updateViewMatrix();
+}
+
+// Private
+void Camera::updateViewMatrix(void){
+  this->ViewMatrix = glm::lookAt(
+    this->camPosition,
+    this->camPosition + this->camFace,
+    this->camUp
+  );
+}
